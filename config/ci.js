fe5a16052400449f036483c6fc99dc4dcaf99d82
davirlein1988 <lein.davir@gmail.com>
2020-03-31 02:57:41 -0500

    clean ignored files

--- config/ci.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    'mongodb://127.0.0.1:27017/blog_ci',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: '',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};

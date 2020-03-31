16e5e16ecd3e8fc67a7a2e3c62fc53be5fa74292
davirlein1988 <lein.davir@gmail.com>
2020-03-31 00:26:34 -0500

    changed db setup

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb+srv://lein1988:Darleida1993@cluster0-kl2sv.mongodb.net/blogster?retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };

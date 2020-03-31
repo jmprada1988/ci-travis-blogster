bce16d6d91ac3458d10cbcc7cb78733778293281
davirlein1988 <lein.davir@gmail.com>
2020-03-31 02:49:35 -0500

    added config to ignore files

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://127.0.0.1:27017',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIATPT74XCUD67FH65L',
+  secretAccessKey: '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',
 };

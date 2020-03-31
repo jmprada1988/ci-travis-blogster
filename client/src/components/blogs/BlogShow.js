c883b41ee7cbcbc31d224f0c2efdf2b483c47326
davirlein1988 <lein.davir@gmail.com>
2020-03-31 04:58:26 -0500

    integrated aws for files upload

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,13 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    console.log(this.props)
+    if (this.props.blog.imageUrl) {
+      return <img src={'https://blogster-bucket-2020.s3-sa-east-1.amazonaws.com/' + this.props.blog.imageUrl} style={{width: '100%'}} />;
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +25,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
---
+++ routes/uploadRoutes.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
+const keys = require('../config/keys');
+
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey
+})
+module.exports = app => {
+  app.get('/api/upload', requireLogin, (req, res, next) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+    s3.getSignedUrl('putObject', {
+      Bucket: 'blogster-bucket-2020',
+      ContentType: 'image/jpeg',
+      Key: key
+    }, (err, url) => res.send({key, url}));
+  })
+}
